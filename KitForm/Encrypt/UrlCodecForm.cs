daed02bb3a81b13928dd0aa167c0389af08b6183
niushuai <niushuai951101@gmail.com>
2022-01-21 13:54:02 +0800

    refactor: url编解码转换

--- KitForm/Encrypt/UrlCodecForm.cs
+++ KitForm/Encrypt/UrlCodecForm.cs
@@ -1,13 +1,5 @@
 ﻿using niushuai233Kit.Util;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 using System.Windows.Forms;
 
 namespace niushuai233Kit.KitForm.Encrypt
@@ -29,7 +21,7 @@ namespace niushuai233Kit.KitForm.Encrypt
             string text = this.encrypt_textBox_decoded.Text;
             if (StringUtil.IsNotEmpty(text))
             {
-                this.encrypt_textBox_encoded.Text = HttpUtility.UrlEncode(text);
+                this.encrypt_textBox_encoded.Text = EncrypyUtil.UrlEncode(text);
             }
         }
 
@@ -43,7 +35,7 @@ namespace niushuai233Kit.KitForm.Encrypt
             string text = this.encrypt_textBox_encoded.Text;
             if (StringUtil.IsNotEmpty(text))
             {
-                this.encrypt_textBox_decoded.Text = HttpUtility.UrlDecode(text);
+                this.encrypt_textBox_decoded.Text = EncrypyUtil.UrlDecode(text);
             }
         }
     }
--- Util/EncrypyUtil.cs
+++ Util/EncrypyUtil.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
 namespace niushuai233Kit.Util
 {
@@ -36,6 +34,26 @@ namespace niushuai233Kit.Util
                 return Convert.ToBase64String(bytes);
             }
             return "";
+        }
+
+        /// <summary>
+        /// url encode
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        internal static string UrlEncode(string text)
+        {
+            return HttpUtility.UrlEncode(text);
+        }
+
+        /// <summary>
+        /// url decode
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        internal static string UrlDecode(string text)
+        {
+            return HttpUtility.UrlDecode(text);
         }
     }
 }
