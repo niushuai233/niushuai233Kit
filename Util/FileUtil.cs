d0954179254635e5f2c602869fa432ccf03ebc6d
niushuai <niushuai951101@gmail.com>
2022-02-08 17:58:38 +0800

    feat: 添加fileutil

---
+++ Util/FileUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace niushuai233Kit.Util
+{
+    public class FileUtil
+    {
+
+        public static List<string> ListDirectory(string path, string pattern = "")
+        {
+            List<string> list = new List<string>();
+
+            if (path == null)
+            {
+                return list;
+            }
+            string dirPath = @path;
+            DirectoryInfo root = new DirectoryInfo(dirPath);
+            FileInfo[] files = root.GetFiles();
+            
+            if (CollectionUtil.IsEmpty(files))
+            {
+                return list;
+            }
+
+            foreach (FileInfo item in files)
+            {
+                if (item.FullName.EndsWith(""))
+                {
+                    list.Add(item.FullName);
+                }
+            }
+
+            return list;
+        }
+    }
+}
