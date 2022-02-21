6b448e89a955ac4da1cf29dfc79bb06bf2308fff
niushuai <niushuai951101@gmail.com>
2022-02-21 10:52:19 +0800

    fix: 调整窗体属性

--- Updater/AutoupdaterForm.Designer.cs
+++ Updater/AutoupdaterForm.Designer.cs
@@ -30,24 +30,58 @@ namespace niushuai233Kit.Updater
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_update = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(16, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(776, 426);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(779, 406);
             this.textBox1.TabIndex = 0;
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(589, 430);
+            this.button_update.Margin = new System.Windows.Forms.Padding(4);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(100, 31);
+            this.button_update.TabIndex = 1;
+            this.button_update.Text = "开始更新";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(697, 430);
+            this.button_close.Margin = new System.Windows.Forms.Padding(4);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(100, 31);
+            this.button_close.TabIndex = 2;
+            this.button_close.Text = "关闭";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
             // 
             // AutoupdaterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(810, 469);
+            this.Controls.Add(this.button_close);
+            this.Controls.Add(this.button_update);
             this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("宋体", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "AutoupdaterForm";
-            this.Text = "AutoupdaterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "检测更新";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +90,7 @@ namespace niushuai233Kit.Updater
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.Button button_close;
     }
 }
--- Updater/AutoupdaterForm.cs
+++ Updater/AutoupdaterForm.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using niushuai233Kit.Entity.Github;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,28 @@ namespace niushuai233Kit.Updater
 {
     public partial class AutoupdaterForm : Form
     {
+        private Releases releases;
         public AutoupdaterForm(Entity.Github.Releases newestReleases)
         {
+            releases = newestReleases;
             InitializeComponent();
-            this.textBox1.Text = JsonConvert.SerializeObject(newestReleases, Formatting.Indented);
+
+            Init();
+        }
+
+        private void Init()
+        {
+            this.textBox1.Text = releases.body;
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
